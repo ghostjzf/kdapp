795c2b9998b2aff276a9136b8e5f8573c50ca413
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2018-12-15 23:17:34 +0800

    feat(API): 增加API设置模块

---
+++ utils/API/apis/KD.js
@@ -0,0 +1,5 @@
+export const KD = {
+  // 用户状态
+  login: `/login`,
+  logout: '/logout'
+};
---
+++ utils/API/index.js
@@ -0,0 +1,9 @@
+import { SSO } from "./apis/SSO.js"
+import { KD } from "./apis/KD.js"
+
+export const HOST = ['https://kd-api-test.kdcity.com', 'https://kd-api.kdcity.com'];
+
+export const API = {
+  SSO,
+  KD
+};
