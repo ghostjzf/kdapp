a8502e134c62f6d84d6d63f1144f842f90b31831
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-01-25 20:13:28 +0800

    feat: merge

--- utils/API/index.js
+++
@@ -1,6 +0,0 @@
-export const HOST = ['http://172.25.6.162:9999', 'https://kd-api.kdcity.com'];
-
-export const API = {
-  "storeList": "/api/customer/store/list",
-  "recoomend": "/api/customer/public/images/store/recommend"
-};
